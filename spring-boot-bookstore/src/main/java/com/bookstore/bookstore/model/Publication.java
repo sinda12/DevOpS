8e6f208c0c74f3bc5214e5222105111b33c8b86f
mabourguiba <mabourguiba@vermeg.com>
2022-05-25 09:42:23 +0100

    AZIZ-1 : preparation model et repo

---
+++ spring-boot-bookstore/src/main/java/com/bookstore/bookstore/model/Publication.java
@@ -0,0 +1,44 @@
+package com.bookstore.bookstore.model;
+
+public class Publication {
+
+
+    private int id_publication ;
+    private int id_blog ;
+    private int id_client ;
+    private String contenue;
+    private String commentaire;
+
+    public int getId_publication() {
+        return id_publication;
+    }
+    public void setId_publication(int id_publication) {
+        this.id_publication = id_publication;
+    }
+
+    public int getId_blog() {
+        return id_blog;
+    }
+    public void setId_blog(int id_blog) {
+        this.id_blog = id_blog;
+    }
+
+    public int getId_client() {
+        return id_client;
+    }
+    public void setId_client(int id_client) {
+        this.id_client = id_client;
+    }
+
+    public String getContenue() {
+        return contenue;
+    }
+
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+    public void setContenue(String commentaire) {
+        this.commentaire = commentaire;
+    }
+}
